860c79fa7b822303d5b99aa741fa396b4ab1583f
qwikfeb-coder <anielrapole@gmail.com>
2025-10-07 15:15:25 +0530

    code

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = file("C:/Users/Dell/Downloads/rock-position-455809-m2-f1f728655974.json")
-  project     = "rock-position-455809-m2"
+    project     = "rock-position-455809-m2"
   region      = var.region
 }
