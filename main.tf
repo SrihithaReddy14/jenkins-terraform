c1a6642ecfe98f2fea077fceb2878018f9cec412
SrihithaReddy14 <146497483+SrihithaReddy14@users.noreply.github.com>
2025-10-16 15:03:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 # 1. VPC Configuration
 resource "google_compute_network" "vpc_network" {
-  name                    = "vpc-network-test"
+  name                    = "vpc-network-test1"
   auto_create_subnetworks = false
 }
 
 # 2. Subnet Configuration
 resource "google_compute_subnetwork" "subnet" {
-  name          = "subnet-1"
+  name          = "subnet-11"
   region        = "us-central1"  # Updated region
   network       = google_compute_network.vpc_network.name
   ip_cidr_range = "10.0.0.0/24"
 }
 
-resource "google_storage_bucket" "sristoragetest" {
-  name = "sristoragetest"
+resource "google_storage_bucket" "sristoragetest1" {
+  name = "sristoragetest1"
   location = "US"
 }
